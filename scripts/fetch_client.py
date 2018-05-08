18ea13fa54ba91040b6063450e3c40b7e9ff7add
Chris Bollinger <cwbollinger@gmail.com>
2018-05-08 13:53:03 -0700

    Temp commit so files can be shared

---
+++ scripts/fetch_client.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+import sys
+import rospy
+import actionlib
+import subprocess
+from long_term_server.msg import *
+from long_term_server.srv import *
+from std_srvs.srv import Empty
+
+
+class LongTermAgentClient(object):
+    def __init__(self):
+        rospy.wait_for_service('register_agent')
+        self.register_agent_proxy = rospy.ServiceProxy('register_agent', RegisterAgent)
+        rospy.wait_for_service('unregister_agent')
+        self.unregister_agent_proxy = rospy.ServiceProxy('unregister_agent', UnregisterAgent)
+        rospy.wait_for_service('get_agents')
+        self.get_agents_proxy = rospy.ServiceProxy('get_agents', GetRegisteredAgents)
+
+    def register_agent(self, a_name, a_type):
+        description = AgentDescription()
+        description.agent_name = a_name
+        description.agent_type = a_type
+        try:
+            resp1 = self.register_agent_proxy(description)
+            return resp1.assigned_name 
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
+            return False 
+
+    def unregister_agent(self, a_name):
+        try:
+            resp1 = self.unregister_agent_proxy(a_name)
+            return resp1.success 
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
+            return False 
+
+    def get_agents(self):
+        try:
+            resp1 = self.get_agents_proxy()
+            return resp1.agents
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
+            return [] 
+
+
+class TaskActionServer(object):
+    # create messages that are used to publish feedback/result
+    _feedback = TaskFeedback()
+    _result = TaskResult()
+
+    def __init__(self, name):
+        self._action_name = name
+        self._as = actionlib.SimpleActionServer(self._action_name, TaskAction, execute_cb=self.execute_cb, auto_start = False)
+        self._as.start()
+      
+    def execute_cb(self, goal):
+        # helper variables
+        success = True
+        print goal
+
+        p = subprocess.Popen(['~/{}/devel/env.sh'.format(goal.workspace_name), 'roslaunch', goal.package_name, goal.launchfile_name])
+
+        self._feedback.status = "This is a test msg."
+        self._as.publish_feedback(self._feedback)
+        
+        # start executing the action
+        while  p.poll() is None:
+            # check that preempt has not been requested by the client
+            if self._as.is_preempt_requested():
+                rospy.loginfo('%s: Preempted' % self._action_name)
+                self._as.set_preempted()
+                p.kill()
+                success = False
+                break
+          
+        if success:
+            self._result.success_msg = "Hooray!"
+            rospy.loginfo('%s: Succeeded' % self._action_name)
+            self._as.set_succeeded(self._result)
+        
+if __name__ == "__main__":
+    server_client = LongTermAgentClient()
+    agent_name = server_client.register_agent('fetch', 'fetch')
+    namespace = '{}_agent'.format(agent_name)
+    rospy.init_node('{}'.format(namespace))
+    task_interface = TaskActionServer(namespace)
+    rospy.spin()
--- scripts/register_agent_server.py
+++ scripts/register_agent_server.py
@@ -1,34 +1,54 @@
 #!/usr/bin/env python
 
+import collections
+
 import rospy
 
 from long_term_server.srv import *
 from long_term_server.msg import *
 
+Client = collections.namedtuple('Client', ['name', 'robot_type', 'action_client'], )
+
 
 class LongTermAgentServer(object):
     def __init__(self):
         self.agents = []
-        self.nh = rospy.init_node('register_agent_server')
+        rospy.init_node('task_server')
         self.s1 = rospy.Service('register_agent', RegisterAgent, self.handle_register_agent)
         self.s2 = rospy.Service('unregister_agent', UnregisterAgent, self.handle_unregister_agent)
         self.s3 = rospy.Service('get_agents', GetRegisteredAgents, self.handle_get_agents)
 
     def handle_register_agent(self, req):
         if req.description not in self.agents:
-            self.agents.append(req.description)
-        return RegisterAgentResponse(True) # always succeed for now
+            print 'registering agent: {}'.format(req.description.agent_name)
+            c = Client(req.description.agent_name, req.description.agent_type, None)
+            c.action_client = actionlib.SimpleActionClient
+            self.agents.append(c)
+            return RegisterAgentResponse(True, req.description.agent_name)
+        return RegisterAgentResponse(False, "")
 
     def handle_unregister_agent(self, req):
-        a_name = req.agent_name
-        names = [a.agent_name for a in self.agents]
+        names = [a.name for a in self.agents]
         if a_name in names:
+            print 'unregistering agent: "{}"'.format(a_name)
             del self.agents[names.index(a_name)]
         return UnregisterAgentResponse(True) # always succeed for now
 
     def handle_get_agents(self, req):
         return GetRegisteredAgentsResponse(self.agents)
-    
+
+    def send_task(self, agent_name, workspace_name, package_name, launchfile_name):
+        names = [a.name for a in self.agents]
+        idx = names.index(agent_name)
+        goal = TaskGoal()
+        print(goal)
+        return
+        '''
+        self.agents[idx].action_client.send_goal
+
+        pass
+        '''
+
 
 if __name__ == "__main__":
     server = LongTermAgentServer()
